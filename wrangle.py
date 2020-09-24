1526367becfa60ec68ed4a5d93f8ef7bf8a00900
Luke Becker <luke.w.becker32@gmail.com>
2020-09-24 11:27:00 -0500

    Added start of wrangle file

---
+++ wrangle.py
@@ -0,0 +1,11 @@
+# File for creating an acquire and prepping of files for regression exercises.
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import env
+
